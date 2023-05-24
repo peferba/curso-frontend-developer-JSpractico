df4d4c7c13456a6f89fec26210016aeff6170f1d
peferba <peferbame@gmail.com>
2023-05-24 09:17:52 -0500

    carrito de compras

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 const menuEmail =document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu"); 
 const menuHamIcon=document.querySelector("#burger-menu");
+const menuCarritoIcon=document.querySelector(".navbar-shopping-cart");
 const mobileMenu=document.querySelector(".mobile-menu");
+const aside = document.querySelector('.product-detail');
 
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
 menuHamIcon.addEventListener('click',toggleMobileMenu);
+menuCarritoIcon.addEventListener('click',togglecarritoicom)
 
 
 function toggleDesktopMenu(){
@@ -14,4 +17,8 @@ function toggleDesktopMenu(){
 
 function toggleMobileMenu(){
     mobileMenu.classList.toggle('inactive');
+}
+
+function togglecarritoicom (){
+    aside.classList.toggle('inactive');
 }
